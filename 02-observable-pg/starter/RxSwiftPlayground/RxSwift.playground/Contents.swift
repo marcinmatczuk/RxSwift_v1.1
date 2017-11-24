63be6b8b0da4a43bb759c262d174d00009161895
Marcin Matczuk <mmatczuk@mmatczuks-iMac.local>
2017-11-24 16:25:27 +0100

    Initial experiments.

--- 02-observable-pg/starter/RxSwiftPlayground/RxSwift.playground/Contents.swift
+++ 02-observable-pg/starter/RxSwiftPlayground/RxSwift.playground/Contents.swift
@@ -1,26 +1,52 @@
 //: Please build the scheme 'RxSwiftPlayground' first
 import RxSwift
 
+example(of: "just, of, from") {
+    // 1
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    // 2
+    let observable: Observable<Int> = Observable<Int>.just(one)
+    let observable2 = Observable.of(one, two, three)
+    let observable3 = Observable.of([one, two, three])
+    let observable4 = Observable.from([one, two, three])
+    
+    observable4.subscribe { event in
+        if let element = event.element {
+            print(element)
+        }
+    }
+}
 
 
-/*:
- Copyright (c) 2014-2016 Razeware LLC
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+example(of: "empty") {
+    let observable = Observable<Void>.empty()
+    
+    observable
+        .subscribe(
+            // 1
+            onNext: { element in print(element)
+                
+        },
+            onCompleted: {
+                print("Completed")
+    }
+    )
+}
+
+example(of: "never") {
+    let observable = Observable<Any>.never()
+    
+    observable
+        .subscribe(
+            // 1
+            onNext: { element in print(element)
+                
+        },
+            onCompleted: {
+                print("Completed")
+        }
+    )
+}
