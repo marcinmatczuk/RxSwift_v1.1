1332ee98e8cf7a209ef5e73d2ba99fe451ca7b4e
Marcin Matczuk <mmatczuk@mmatczuks-iMac.local>
2017-11-28 16:13:33 +0100

    Implemented challenges from ch3 about variables.

--- 03-subjects-and-variable/starter/RxSwiftPlayground/RxSwift.playground/Contents.swift
+++ 03-subjects-and-variable/starter/RxSwiftPlayground/RxSwift.playground/Contents.swift
@@ -4,26 +4,64 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 import RxSwift
 
+public func example(of description: String, action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
+}
+
+enum MyError: Error {
+    case anError
+}
+
+func print<T: CustomStringConvertible>(label: String, event: Event<T>) {
+    print(label, event.element ?? event.error ?? event)
+}
+
+example(of: "BehaviorSubject") {
+//    let subject = BehaviorSubject(value: "Initial value")
+    let subject = ReplaySubject<String>.create(bufferSize: 2)
+    let disposeBag = DisposeBag()
+    
+    subject.onNext("X")
+    subject.onNext("Y")
+    
+    subject
+        .subscribe {
+            print(label: "1)", event: $0)
+        }
+        .disposed(by: disposeBag)
+    
+    subject.onError(MyError.anError)
+    
+    subject.dispose()
+    
+    subject
+        .subscribe {
+            print(label: "2)", event: $0)
+        }
+        .disposed(by: disposeBag)
+}
 
 
-/*:
- Copyright (c) 2014-2016 Razeware LLC
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- */
+example(of: "Variable") {
+    let variable = Variable("initial value")
+    let disposeBag = DisposeBag()
+    
+    variable.value = "new initial value"
+    
+    variable.asObservable()
+        .subscribe {
+            print(label: "1)", event: $0)
+        }
+        .disposed(by: disposeBag)
+    
+    variable.value = "1"
+    
+    variable.asObservable()
+        .subscribe {
+            print(label: "2)", event: $0)
+        }
+        .disposed(by: disposeBag)
+    
+    variable.value = "2"
+}
